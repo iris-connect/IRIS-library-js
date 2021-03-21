0463a63ad80469202a60afb11598163bca2577d5
Bernhard Wittmann <dev@bernhardwittmann.com>
2021-03-21 02:09:38 +0100

    test: disable test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,4 @@ module.exports = {
   transform: { '\\.ts$': ['ts-jest'] },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,js}'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
-    },
-  },
 };
